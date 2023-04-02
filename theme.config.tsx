abd65773a2da5a11a9ab7f9c28ad599aaedcd230
dujesa <dujes@codemage.co>
2023-04-02 16:24:48 +0200

    Tweaked theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Dump Internship dev</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/dujesa/blog",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://github.com/dujesa",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/dujesa/blog",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Dump Internship dev - Web Api",
   },
-}
+};
 
-export default config
+export default config;
